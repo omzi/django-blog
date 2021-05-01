dc11b8197c7b3c3d9b3e20b0fc243a37c740dc11
omzi <obiohaomezibe@gmail.com>
2021-05-01 18:31:29 +0100

    Added django-heroku

--- blog/settings.py
+++ blog/settings.py
@@ -1,6 +1,7 @@
 from environs import Env
 env = Env()
 env.read_env()
+import django_heroku
 
 
 import os
@@ -153,3 +154,4 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
